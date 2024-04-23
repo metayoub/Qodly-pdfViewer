96192646fbf9754710dcbb9e429480b8f500e086
AyaBengherifa <aya.bengherifa@4d.com>
2024-04-23 16:17:03 +0100

    pdf viewer component

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Qodly-pdfViewer
+# Overview
+
+pdfViewer component for Qodly studio
+
+## PdfViewer
+
+An efficient PDF viewer component with customizable features
+![pdfViewer](C:\Users\HP\Desktop\qodly_pdfviewer\public\pdfViewer.png)
+
+## Properties
+
+| Name    | Type   | Description                         |
+| ------- | ------ | ----------------------------------- |
+| pdfLink | string | property to represent the pdf link. |
---
+++ src/components/PdfViewer/PdfViewer.config.tsx
@@ -0,0 +1,66 @@
+import { EComponentKind, T4DComponentConfig } from '@ws-ui/webform-editor';
+import { Settings } from '@ws-ui/webform-editor';
+
+import { FaRegFilePdf } from 'react-icons/fa6';
+
+import PdfViewerSettings, { BasicSettings } from './PdfViewer.settings';
+
+export default {
+  craft: {
+    displayName: 'PdfViewer',
+    kind: EComponentKind.BASIC,
+    props: {
+      name: '',
+      classNames: [],
+      events: [],
+    },
+    related: {
+      settings: Settings(PdfViewerSettings, BasicSettings),
+    },
+  },
+  info: {
+    displayName: 'PdfViewer',
+    exposed: true,
+    icon: FaRegFilePdf,
+    events: [
+      {
+        label: 'On Click',
+        value: 'onclick',
+      },
+      {
+        label: 'On Blur',
+        value: 'onblur',
+      },
+      {
+        label: 'On Focus',
+        value: 'onfocus',
+      },
+      {
+        label: 'On MouseEnter',
+        value: 'onmouseenter',
+      },
+      {
+        label: 'On MouseLeave',
+        value: 'onmouseleave',
+      },
+      {
+        label: 'On KeyDown',
+        value: 'onkeydown',
+      },
+      {
+        label: 'On KeyUp',
+        value: 'onkeyup',
+      },
+    ],
+    datasources: {
+      accept: ['string'],
+    },
+  },
+  defaultProps: {
+    pdfLink: '',
+  },
+} as T4DComponentConfig<IPdfViewerProps>;
+
+export interface IPdfViewerProps extends webforms.ComponentProps {
+  pdfLink?: any;
+}
